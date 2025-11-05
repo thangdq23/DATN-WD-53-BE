b17e7d25b136683598e561eece09ea5339906b16
ThinhKhanh <thinhkhanh07@gmail.com>
2025-11-05 22:56:40 +0700

    CRUD: movie, cap nhat lai phan movie, them api vao duong dan

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRoute from "./modules/auth/auth.routes.js";
 import genreRoute from "./modules/genre/genre.routes.js";
+import movieRoute from "./modules/movie/movie.routes.js";
 
 const routes = Router();
 
 routes.use("/auth", authRoute);
 routes.use("/genre", genreRoute);
+routes.use("/movie", movieRoute);
 
 export default routes;
