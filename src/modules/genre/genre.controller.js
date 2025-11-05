e7d19732497b20bc65ac838e054fea6a546c4c99
tienthang0810 <email_github_cua_ban@example.com>
2025-11-05 13:28:39 +0700

    update genre

--- src/modules/genre/genre.controller.js
+++ src/modules/genre/genre.controller.js
@@ -4,7 +4,21 @@ import {
   createGenreService,
   updateGenreService,
   updateStatusGenreService,
+  getAllGenreService,
+  getDetailGenreService,
 } from "./genre.service.js";
+
+export const getAllGenre = handleAsync(async (req, res) => {
+  const { query } = req;
+  const data = await getAllGenreService(query);
+  return createResponse(res, 200, "OK", data.data, data.meta);
+});
+
+export const getDetailGenre = handleAsync(async (req, res) => {
+  const { id } = req.params;
+  const data = await getDetailGenreService(id);
+  return createResponse(res, 200, "OK", data);
+});
 
 export const createGenre = handleAsync(async (req, res) => {
   const { body } = req;
