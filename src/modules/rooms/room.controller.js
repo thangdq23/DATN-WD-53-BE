8aa48456870866fb1937f3c7c1ca5790ca3883eb
ThinhKhanh <thinhkhanh07@gmail.com>
2025-11-18 10:32:53 +0700

    Api lay danh sách ghe theo phòng chieu, lay danh sach tat ca phong chieu, cap nhat trang thai (status) cua phong chieu theo ID

--- src/modules/rooms/room.controller.js
+++ src/modules/rooms/room.controller.js
@@ -1,6 +1,31 @@
 import handleAsync from "../../common/utils/async-handler.js";
 import createResponse from "../../common/utils/create-response.js";
-import { createRoomService, updateRoomService } from "./room.service.js";
+import {
+  createRoomService,
+  getAllRoomService,
+  getDetailRoomService,
+  getSeatByRoomService,
+  updateRoomService,
+  updateRoomStatusService,
+} from "./room.service.js";
+
+export const getSeatByRoom = handleAsync(async (req, res) => {
+  const { roomId } = req.params;
+  const data = await getSeatByRoomService(roomId);
+  return createResponse(res, 200, "OK", data);
+});
+
+export const getAllRoom = handleAsync(async (req, res) => {
+  const { query } = req;
+  const data = await getAllRoomService(query);
+  return createResponse(res, 200, "OK", data.data, data.meta);
+});
+
+export const getDetailRoom = handleAsync(async (req, res) => {
+  const { id } = req.params;
+  const data = await getDetailRoomService(id);
+  return createResponse(res, 200, "OK", data);
+});
 
 export const createRoom = handleAsync(async (req, res) => {
   const { body } = req;
@@ -13,4 +38,10 @@ export const updateRoom = handleAsync(async (req, res) => {
   const { body } = req;
   const data = await updateRoomService(id, body);
   return createResponse(res, 200, "Cập nhật phòng chiếu thành công", data);
+});
+
+export const updateStatusRoom = handleAsync(async (req, res) => {
+  const { id } = req.params;
+  const data = await updateRoomStatusService(id);
+  return createResponse(res, 200, data.message, data.data);
 });
