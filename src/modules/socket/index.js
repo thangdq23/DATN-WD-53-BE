96482df8d1cca3972465d20c7680553b59ee4d53
ThinhKhanh <thinhkhanh07@gmail.com>
2025-12-04 02:00:04 +0700

    config socket

--- src/modules/socket/index.js
+++ src/modules/socket/index.js
@@ -1,9 +1,9 @@
 import { Server } from "socket.io";
-import { socketConfig } from "../../common/configs/socket";
-import { setIO } from "./socket.instance";
-import authSocketMiddleware from "./middlewares/auth.socket.middleware";
-import seatSocketHandler from "./modules/seat.socket.handler";
-import { clearExpiredSeatHoldService } from "../seat-Status/seatStatus.service";
+import { socketConfig } from "../../common/configs/socket.js";
+import { setIO } from "./socket.instance.js";
+import authSocketMiddleware from "./middlewares/auth.socket.middleware.js";
+import seatSocketHandler from "./modules/seat.socket.handler.js";
+import { clearExpiredSeatHoldService } from "../seat-Status/seatStatus.service.js";
 
 export const initSocket = (httpServer) => {
   const io = new Server(httpServer, socketConfig);
