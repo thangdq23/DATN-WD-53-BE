d67f9391a9eef0a39eead5369d7ce9231a489329
tienthang0810 <tienthang0810@gmail.com>
2025-12-10 23:57:47 +0700

    cap nhat user controler

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -1,22 +1,46 @@
 import handleAsync from "../../common/utils/async-handler.js";
 import createResponse from "../../common/utils/create-response.js";
 import {
+  changePasswordService,
   createUserService,
   getAllUserService,
+  getDetailUserService,
+  getMyDetailTicketService,
+  getMyticketService,
+  getProfileService,
+  updateBlockUserService,
+  updateProfileService,
   updateUserService,
 } from "./user.service.js";
 
-export const getAllUser = handleAsync(async (req, res) => {
-  const users = await getAllUserService();
-  return createResponse(res, 200, "Lấy danh sách thành công!", users);
+export const getProfile = handleAsync(async (req, res) => {
+  const { _id } = req.user;
+  const response = await getProfileService(_id);
+  return createResponse(res, 200, "OK", response);
 });
 
-export const createUser = handleAsync(async (req, res) => {
-  const users = await createUserService(req.body);
-  return createResponse(res, 201, "Tạo user thành công!", users);
+export const updateProfile = handleAsync(async (req, res) => {
+  const { _id } = req.user;
+  const response = await updateProfileService(req.body, _id);
+  return createResponse(res, 200, "Cập nhật thông tin thành công!", response);
 });
 
-export const updateUser = handleAsync(async (req, res) => {
-  const users = await updateUserService(req.params.id, req.body);
-  return createResponse(res, 200, "Cập nhật user thành công", users);
+export const changePassword = handleAsync(async (req, res) => {
+  const { _id } = req.user;
+  const response = await changePasswordService(req.body, _id);
+  return createResponse(res, 200, "Đổi mật khẩu thành công!", response);
+});
+
+export const getMyTicket = handleAsync(async (req, res) => {
+  const { _id } = req.user;
+  const response = await getMyticketService(_id, req.query);
+  return createResponse(res, 200, "OK", response.data, response.meta);
+});
+
+export const getDetailMyTicket = handleAsync(async (req, res) => {
+  const { ticketId } = req.params;
+  const { _id } = req.user;
+  const response = await getMyDetailTicketService(_id, ticketId);
+  console.log(response);
+  return createResponse(res, 200, "OK", response);
 });
