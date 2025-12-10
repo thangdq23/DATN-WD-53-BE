6d796c397e6af6513cb46bdecde53c1cfc6e3631
tienthang0810 <tienthang0810@gmail.com>
2025-12-11 00:23:33 +0700

    them file auth messages

---
+++ src/modules/auth/auth.messages.js
@@ -0,0 +1,20 @@
+export const AUTH_MESSAGES = {
+  CONFLICT_EMAIL: "Địa chỉ email này đã tồn tại!",
+  CONFLICT_NAME: "Tên người dùng này đã tồn tại!",
+  CONFLICT_PHONE: "Số điện thoại này đã tồn tại!",
+  REGISTER_SUCCESS:
+    "Đăng ký thành công vui lòng kiểm tra email để xác thực tài khoản!",
+  NOTFOUND_EMAIL: "Địa chỉ email này chưa được đăng ký!",
+  WRONG_PASSWORD: "Thông tin đăng nhập không chính xác!",
+  NOT_VERIFIED: "Tài khoản của bạn chưa được xác thực!",
+  IS_VERIFIED: "Tài khoản của bạn đã được xác thực!",
+  LOGIN_SUCCESS: "Đăng nhập thành công!",
+  SEND_VERIFY_SUCCESS:
+    "Vui lòng kiểm tra email chúng tôi đã gửi mã xác thực tới tài khoản của bạn",
+  RESETPASS_SUCCESS:
+    "Vui lòng kiểm tra email chúng tôi đã gửi mật khẩu mới tới email của bạn!",
+  VERIFIED_SUCCESS: "Xác thực tài khoản thành công!",
+  NOTFOUND_USER: "Không tìm thấy tài khoản của bạn!",
+  REQUIRE_GOOLE:
+    "Tài khoản của bạn chưa có mật khẩu vui lòng đăng nhập bằng google!",
+};
