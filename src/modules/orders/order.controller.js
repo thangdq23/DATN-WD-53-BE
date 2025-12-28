fc243a121e210b5e4415179ba0efc379e267e51f
tienthang0810 <tienthang0810@gmail.com>
2025-12-28 23:28:52 +0700

    tao link thanh toan payos

--- src/modules/orders/order.controller.js
+++ src/modules/orders/order.controller.js
@@ -5,12 +5,14 @@ import { getMyOrdersService } from "./order.service.js";
 
 export const checkout = handleAsync(async (req, res) => {
   const { body } = req;
+  const userId = req.user._id;
+  if (userId) body.userId = userId;
   const data = await checkoutService(body);
   return createResponse(res, 201, "Đặt vé thành công", data);
 });
 
 export const getMyOrders = handleAsync(async (req, res) => {
-  const { userId } = req.params;
+  const userId = req.user._id;
   const { query } = req;
   const { data, meta } = await getMyOrdersService(userId, query);
   return createResponse(res, 200, "OK", data, meta);
--- src/modules/orders/order.routes.js
+++ src/modules/orders/order.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { checkout, getMyOrders } from "./order.controller.js";
+import { authenticate } from "../../common/middlewares/auth.middleware.js";
+import { JWT_ACCESS_SECRET } from "../../common/configs/environment.js";
 
-const checkoutRoute = Router();
-checkoutRoute.post("/", checkout);
-checkoutRoute.get("/my/:userId", getMyOrders);
+const orderRoute = Router();
+orderRoute.post("/", authenticate(JWT_ACCESS_SECRET), checkout);
+orderRoute.get("/my", authenticate(JWT_ACCESS_SECRET), getMyOrders);
 
-export default checkoutRoute;
+export default orderRoute;
