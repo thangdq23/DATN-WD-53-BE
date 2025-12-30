faf8d0e53179eb2b56b574d9d977eea91049ffb2
tienthang0810 <tienthang0810@gmail.com>
2025-12-30 09:39:35 +0700

    Them id khi response return va API getDetailOrder

--- src/modules/orders/order.routes.js
+++ src/modules/orders/order.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   checkout,
   checkoutReturnPayos,
+  getDetailOrder,
   getMyOrders,
 } from "./order.controller.js";
 import { authenticate } from "../../common/middlewares/auth.middleware.js";
@@ -10,5 +11,6 @@ import { JWT_ACCESS_SECRET } from "../../common/configs/environment.js";
 const orderRoute = Router();
 orderRoute.post("/", authenticate(JWT_ACCESS_SECRET), checkout);
 orderRoute.get("/my", authenticate(JWT_ACCESS_SECRET), getMyOrders);
+orderRoute.get("/detail/:id", getDetailOrder);
 orderRoute.get("/return", checkoutReturnPayos);
 export default orderRoute;
